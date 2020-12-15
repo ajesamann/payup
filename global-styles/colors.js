2c5b643302437053e191c240bc5e67d74dd420ea
AJ Esamann <56281731+ajesamann@users.noreply.github.com>
2020-12-14 23:56:34 -0500

    Update colors.js

--- global-styles/colors.js
+++ global-styles/colors.js
@@ -2,5 +2,7 @@ export const appColors = {
     primary: '#00204a',
     lightPrimary: '#005792',
     lightestPrimary: '#00bbf0',
+    appPurple: '#4d0094',
+    appBlue: '#3f5cff',
     error: '#fdb44b'
 }
